8a44a252d24a240ef0a9474e391a8993c6095314
AntalMate <antalm@kkszki.hu>
2026-09-23 18:41:29 +0200

    beolvasás, mentés listába

--- repulokOroklodes/Program.cs
+++ repulokOroklodes/Program.cs
@@ -1,10 +1,42 @@
 ﻿namespace repulokOroklodes
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] sorok = File.ReadAllLines("gepek.txt");
+            List<Repulo> gepek=new List<Repulo>();
+            //beolvasás mentés listába
+            foreach (string s in sorok)
+            {
+                try
+                {
+                    string[] adatok = s.Split(";");
+                    string lajstrom = adatok[1];
+                    string nev=adatok[2];
+                    List<string> felszereltsegek = adatok[3].Split(",").ToList();
+                    int ertek = int.Parse(adatok[4]);
+                    if (adatok[0] == "U")
+                    {
+                        int ulesek=int.Parse(adatok[5]);
+                        gepek.Add(new UtasszallitoGep(lajstrom, nev, felszereltsegek, ertek, ulesek));
+                    }
+                    else if(adatok[0] == "T")
+                    {
+                        int maxKg=int.Parse(adatok[5]);
+                        gepek.Add(new TeherszallitoGep(lajstrom,nev,felszereltsegek,ertek,maxKg));
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            foreach (var elem in gepek)
+            {
+                Console.WriteLine(elem);
+            }
+
         }
     }
 }
